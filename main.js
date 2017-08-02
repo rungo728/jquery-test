1ff65b87af90bfe3b7aabe2425e218e979a5dc61
Hayato oyanagi <oyana@oya.local>
2017-08-02 11:24:18 +0900

    edit

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 $(function(){
+  // 処理の説明を入れてください
+  //　例：ボタンが押されると、sub-titleがremoveされる。
   $("#hideButton").on("click", function(){
     $('.sub-title').remove();
   });
-  $("#removeClassButton").on('click', function(){
-    $('.red').removeClass('red')
-  });
-  $('#addClassButton').on('click', function(){
-    $("h2").addClass('blue')
-  });
-  // $(window).scroll(function(){
-  //   console.log(1);
-  // })
-  var height = 0
-  $(window).scroll(function(){
-    height += 1
-    console.log(height)
-    if (height >= 50){
-      console.log("fasdfas")
-    }
-  })
 });
